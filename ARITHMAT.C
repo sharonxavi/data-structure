dafc256a18580db4d79ec2d4a67345bc51b4a439
sharonxavi <124430827+sharonxavi@users.noreply.github.com>
2023-05-05 14:33:28 +0530

    Add files via upload

---
+++ ARITHMAT.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("Addition",c);
+printf("subtraction",d);
+printf("multiplication",e);
+printf("division",f);
+}
